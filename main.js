37fcd577467cfe90c4baa683fb076f76467b8c79
nidhirvnair <73595631+nidhirvnair@users.noreply.github.com>
2021-02-24 15:05:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+ var canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=3;
+ctx.rect(150,143,430,200);
+ctx.stroke();
